98e75a99aa88a86992a900c9a5c6b301843788a7
Jinsujin <sylpid003@gmail.com>
2021-06-19 10:24:18 +0900

    # 다리 위 트럭 (테스트 케이스 통과, 코드 개선필요) - 큐에 들어갈 구조체 선언 	Truck구조체는 다리위에 올라갔을때 시간을 값으로 가짐 - while 루프가 돌때마다 시간+=1 - 다리위트럭이 있다면, 현재 시간과 비교해서 도착했는지 체크 	도착했다면, 다리위트럭큐에서 디큐 - 다리위에 트럭을 놓을 수 있는지 체크 	놓을 수 있다면 트럭큐에서 디큐하여, 다리위트럭큐에 인큐

--- Playgrounds/MyPlayground.playground/Pages/TruckOnBridge.xcplaygroundpage/Contents.swift
+++ Playgrounds/MyPlayground.playground/Pages/TruckOnBridge.xcplaygroundpage/Contents.swift
@@ -7,28 +7,28 @@ import Foundation
  */
 
 struct Truck {
-    let idx: Int
-    var 다리위에올라온시간: Int = 0
+    let weight: Int
+    var startTime: Int = 0
 }
 
 struct Queue {
-    private var queue: [Int] = []
+    public var queue: [Truck] = []
     
-    public mutating func enqueue(_ element: Int) {
+    public mutating func enqueue(_ element: Truck) {
         queue.append(element)
     }
     
-    public mutating func dequeue() -> Int? {
+    public mutating func dequeue() -> Truck? {
         return isEmpty ? nil : queue.removeFirst()
     }
     
-    public func first() -> Int? {
+    public func first() -> Truck? {
         return queue.first
     }
     
     // 전체 큐 데이터의 합
     public var totalSum: Int {
-        return queue.reduce(0, +)
+        return queue.map({$0.weight}).reduce(0, +)
     }
     
     public var count: Int {
@@ -59,63 +59,60 @@ let truck_weights = [7,4,5,6]
 
 
 
+solution(bridge_length, weight, truck_weights)
+
+
+
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    
+    var time = 0
     // 움직여야 할 트럭들
     var trucksQueue = Queue()
     for t in truck_weights {
-        trucksQueue.enqueue(t)
+        trucksQueue.enqueue(Truck(weight: t, startTime: time))
     }
-    
-    let trucksCount = trucksQueue.count
     
     // 다리위 트럭들 - bridge_length 갯수만큼 가질 수 있다
     var trucksOnBridgeQueue = Queue()
     
-    var time = 0
-    
-//    var goalTrucks = Queue()
-    
-    
-    // 옮길 트럭이 존재하고, 다리 위 트럭이 아무 것도 없는 경우
-    while(!trucksQueue.isEmpty || trucksOnBridgeQueue.isEmpty == false) {
-//    while(goalTrucks.count < trucksCount ) {
+    // 큐에 값이 하나라도 있으면 루프를 계속 돈다
+    while(!trucksQueue.isEmpty || !trucksOnBridgeQueue.isEmpty) {
         print("time ==== ", time)
-        // 1. 다리위에 트럭을 놓을 수 있나. 시간 1 소요
+        
+        // 1. 다리위에 올라간 트럭이 있으면 체크
+        if !trucksOnBridgeQueue.isEmpty {
+            var goalCount = 0
+            for t in trucksOnBridgeQueue.queue {
+                if time >= (t.startTime + bridge_length) {
+                    goalCount+=1
+                }
+            }
+            
+            for _ in 0..<goalCount {
+                trucksOnBridgeQueue.dequeue()
+            }
+        }
+        
+        // 2. 다리위에 트럭을 놓을 수 있나
         if let currentTruck = trucksQueue.first(),
-           currentTruck <= (weight - trucksOnBridgeQueue.totalSum),
+           currentTruck.weight <= (weight - trucksOnBridgeQueue.totalSum),
            trucksOnBridgeQueue.count < bridge_length {
             
             if let truck = trucksQueue.dequeue() {
                    // 다리에 트럭을 올릴 수 있는 상태. 트럭을 하나 빼서 다리위에 올린다
-                   trucksOnBridgeQueue.enqueue(truck)
+                let t = Truck(weight: truck.weight, startTime: time)
+                   trucksOnBridgeQueue.enqueue(t)
                    print("다리위에 올라간 트럭 ", truck)
-                   time += 1
-                   continue
                }
-        } else {
-
-            // Error::-
-            // 2. 다리위 트럭이 있나 -> 트럭 옮기기(다리 길이만큼 시간이 소요된다)
-            // 트럭이 하나라도 다리 위에 있고, 다리 위에 더이상 트럭을 올릴수 없을때 옮긴다
-            // 트럭이 다리위에 시간텀을 두고 올라왔을떄 어떻게 처리할 것인지 - 트럭이 몇초에 다리위로 올라왔는지 기록해둔다..?
-            if !trucksOnBridgeQueue.isEmpty {
-                print("완료된 트럭 수" , trucksOnBridgeQueue.count)
-                for _ in 0..<trucksOnBridgeQueue.count {
-                    trucksOnBridgeQueue.dequeue()
-                }
-                time += bridge_length
-            }
         }
 
-
-        
-
-    }
+        time += 1
+    } // End while
     
     return time
 }
 
 
-solution(bridge_length, weight, truck_weights)
+
 
 //: [Next](@next)
